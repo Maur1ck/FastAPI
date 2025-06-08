82ef31dc07c538a6f75f375623358b837843f7a9
comander <misha20061508@yandex.ru>
2025-06-08 19:09:42 +0300

    create database.py, add DB_URL to settings

--- app/main.py
+++ app/main.py
@@ -7,9 +7,6 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from app.api.hotels import router as router_hotels
-from app.config import settings
-
-print(f"{settings.DB_NAME=}")
 
 app = FastAPI()
 app.include_router(router_hotels)
