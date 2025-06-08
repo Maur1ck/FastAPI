7e49be75ba136130374799b65a1402fac7f42d9d
comander <misha20061508@yandex.ru>
2025-06-08 20:13:22 +0300

    add HotelsOrm & class Base

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
-import asyncio
-
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
 engine = create_async_engine(settings.DB_URL)
 
 new_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
