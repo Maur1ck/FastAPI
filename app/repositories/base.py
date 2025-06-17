031d10803737362e771c4c0b23f55cbda2b64ad1
comander <misha20061508@yandex.ru>
2025-06-17 16:41:21 +0300

    update 'add' method for BaseRepositories

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select, insert
+from pydantic import BaseModel
 
 
 class BaseRepository:
@@ -17,7 +18,7 @@ class BaseRepository:
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
 
-    async def add(self, data):
-        query = insert(self.model).values(**data).returning(self.model)
-        result = await self.session.execute(query)
+    async def add(self, data: BaseModel):
+        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        result = await self.session.execute(add_data_stmt)
         return result.scalar_one()
