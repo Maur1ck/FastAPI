28db999d96860ea8eabac1b674777e9041ea1aa4
comander <misha20061508@yandex.ru>
2025-06-23 17:05:50 +0300

    add hashed_password generation

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter
 
+from passlib.context import CryptContext
+
 from app.database import async_session_maker
-from app.models.rooms import RoomsORM
 from app.repositories.users import UsersRepository
 from app.schemas.users import UserRequestAdd, UserAdd
 
 router = APIRouter(prefix="/auth", tags=["Аутентификация и авторизация"])
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd
 ):
-    hashed_password = data.hashed_password
+    hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        user = await UsersRepository(session).add(new_user_data)
+        await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {"status": "OK"}
--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from app.api.hotels import router as router_hotels
+from app.api.auth import router as router_auth
 
 app = FastAPI()
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
 
