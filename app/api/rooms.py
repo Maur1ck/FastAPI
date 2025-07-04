f2ac592ff430811002a5eb22b2066c2d5579c9b4
comander <misha20061508@yandex.ru>
2025-07-04 10:39:49 +0300

    create get_filtered_by_time() in RoomsRepository

--- app/api/rooms.py
+++ app/api/rooms.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from datetime import date
+
+from fastapi import APIRouter, Query
 
 from app.api.dependencies import DBDep
 from app.schemas.rooms import RoomAdd, RoomPATCH, RoomAddRequest
@@ -7,8 +9,13 @@ router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_rooms(hotel_id: int, db: DBDep):
-    return await db.rooms.get_filtered(hotel_id=hotel_id)
+async def get_rooms(
+        hotel_id: int,
+        db: DBDep,
+        date_from: date = Query(example="2025-08-01"),
+        date_to: date = Query(example="2025-08-10"),
+):
+    return await db.rooms.get_filtered(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
--- app/repositories/rooms.py
+++ app/repositories/rooms.py
@@ -1,5 +1,6 @@
-from sqlalchemy import insert, select
+from sqlalchemy import select, func
 
+from app.models.bookings import BookingsORM
 from app.models.rooms import RoomsORM
 from app.repositories.base import BaseRepository
 from app.schemas.rooms import Room
@@ -8,3 +9,31 @@ from app.schemas.rooms import Room
 class RoomsRepository(BaseRepository):
     model = RoomsORM
     schema = Room
+
+    async def get_filtered_by_time(self, hotel_id, date_from, date_to):
+        rooms_count = (
+            select(BookingsORM.room_id, func.count("*").label("rooms_booked"))
+            .select_from(BookingsORM)
+            .filter(
+                BookingsORM.date_from <= date_to,
+                BookingsORM.date_to >= date_from
+            )
+            .group_by(BookingsORM.room_id)
+            .cte(name="rooms_count")
+        )
+
+        rooms_left_table = (
+            select(
+                self.model.id.label("room_id"),
+                (self.model.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left")
+            )
+            .select_from(self.model)
+            .outerjoin(rooms_count, self.model.id == rooms_count.c.room_id)
+            .cte(name="rooms_left_table")
+        )
+
+        query = (
+            select(rooms_left_table)
+            .select_from(rooms_left_table)
+            .filter(rooms_left_table.c.room_left > 0)
+        )
