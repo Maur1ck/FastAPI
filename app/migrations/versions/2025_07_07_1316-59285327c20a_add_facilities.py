bef8861366b9e522bea36dddf903cae6d7903f31
comander <misha20061508@yandex.ru>
2025-07-07 13:19:06 +0300

    add facilities models (add version file)

---
+++ app/migrations/versions/2025_07_07_1316-59285327c20a_add_facilities.py
@@ -0,0 +1,41 @@
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "59285327c20a"
+down_revision: Union[str, None] = "f65db502d2a3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "room_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("room_facilities")
+    op.drop_table("facilities")
