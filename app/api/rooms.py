106a43e5cfaeedd0138e8ca5d02b74f3f2d9976a
comander <misha20061508@yandex.ru>
2025-07-07 20:09:52 +0300

    edit facilities in PUT /rooms and PATCH /rooms

--- app/api/rooms.py
+++ app/api/rooms.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Query
 
 from app.api.dependencies import DBDep
 from app.schemas.facilities import RoomFacilityAdd
-from app.schemas.rooms import RoomAdd, RoomPATCH, RoomAddRequest
+from app.schemas.rooms import RoomAdd, RoomPATCH, RoomAddRequest, RoomPatchRequest
 
 router = APIRouter(prefix="/hotels", tags=["Номера"])
 
@@ -41,7 +41,27 @@ async def change_room(
         db: DBDep,
         room_data: RoomAddRequest
 ):
-    await db.rooms.edit(room_data, hotel_id=hotel_id, id=room_id)
+    _room_data = RoomAddRequest(
+        title=room_data.title,
+        description=room_data.description,
+        price=room_data.price,
+        quantity=room_data.quantity,
+    )
+    await db.rooms.edit(_room_data, hotel_id=hotel_id, id=room_id, exclude_unset=True)
+
+    facilities = await db.rooms_facilities.get_filtered(room_id=room_id)
+    facilities_ids = [facility.model_dump().get("facility_id") for facility in facilities]
+
+    facilities_for_delete = set(facilities_ids) - set(room_data.facilities_ids)
+    if facilities_for_delete:
+        for f_id in facilities_for_delete:
+            await db.rooms_facilities.delete(room_id=room_id, facility_id=f_id)
+
+    facilities_for_add = set(room_data.facilities_ids) - set(facilities_ids)
+    if facilities_for_add:
+        room_facilities_data_for_add = [RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in facilities_for_add]
+        await db.rooms_facilities.add_bulk(room_facilities_data_for_add)
+
     await db.commit()
     return {"status": "OK"}
 
@@ -51,9 +71,26 @@ async def changes_room(
         hotel_id: int,
         room_id: int,
         db: DBDep,
-        room_data: RoomPATCH
+        room_data: RoomPatchRequest
 ):
-    await db.rooms.edit(room_data, hotel_id=hotel_id, id=room_id, exclude_unset=True)
+    _room_data = RoomPATCH(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
+    await db.rooms.edit(_room_data, hotel_id=hotel_id, id=room_id, exclude_unset=True)
+
+    if room_data.facilities_ids:
+        facilities = await db.rooms_facilities.get_filtered(room_id=room_id)
+        facilities_ids = [facility.model_dump().get("facility_id") for facility in facilities]
+
+        facilities_for_delete = set(facilities_ids) - set(room_data.facilities_ids)
+        if facilities_for_delete:
+            for f_id in facilities_for_delete:
+                await db.rooms_facilities.delete(room_id=room_id, facility_id=f_id)
+
+        facilities_for_add = set(room_data.facilities_ids) - set(facilities_ids)
+        if facilities_for_add:
+            room_facilities_data_for_add = [RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in
+                                            facilities_for_add]
+            await db.rooms_facilities.add_bulk(room_facilities_data_for_add)
+
     await db.commit()
     return {"status": "OK"}
 
--- app/schemas/rooms.py
+++ app/schemas/rooms.py
@@ -22,8 +22,16 @@ class Room(RoomAdd):
     hotel_id: int
 
 
+class RoomPatchRequest(BaseModel):
+    title: str| None = None
+    description: str | None = None
+    price: int| None = None
+    quantity: int| None = None
+    facilities_ids: list[int] | None = None
+
 class RoomPATCH(BaseModel):
-    title: str | None = Field(None)
-    description: str | None = Field(None)
-    price: int | None = Field(None)
-    quantity: int | None = Field(None)
+    hotel_id: int| None = None
+    title: str| None = None
+    description: str | None = None
+    price: int| None = None
+    quantity: int| None = None
