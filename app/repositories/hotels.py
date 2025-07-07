47e41832f8ce87c301e398cec5b6c77ec34813dc
comander <misha20061508@yandex.ru>
2025-07-07 13:03:06 +0300

    add filtering and pagination in HotelsRepository.get_filtered_by_time()

--- app/repositories/hotels.py
+++ app/repositories/hotels.py
@@ -11,12 +11,18 @@ class HotelsRepository(BaseRepository):
     model = HotelsORM
     schema = Hotel
 
-    async def get_all(self, location, title, limit, offset):
-        query = select(HotelsORM)
+    async def get_filtered_by_time(self, date_from, date_to, location, title, limit, offset):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsORM.hotel_id)
+            .select_from(RoomsORM)
+            .filter(RoomsORM.id.in_(rooms_ids_to_get))
+        )
+        query =  select(self.model).filter(self.model.id.in_(hotels_ids_to_get))
         if title:
-            query = query.where(func.lower(HotelsORM.title).contains(title.strip().lower()))
+            query = query.filter(func.lower(HotelsORM.title).contains(title.strip().lower()))
         if location:
-            query = query.where(func.lower(HotelsORM.location).contains(location.strip().lower()))
+            query = query.filter(func.lower(HotelsORM.location).contains(location.strip().lower()))
         query = (
             query
             .limit(limit)
@@ -24,12 +30,3 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return [self.schema.model_validate(model, from_attributes=True) for model in result.scalars().all()]
-
-    async def get_filtered_by_time(self, date_from, date_to):
-        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
-        hotels_ids_to_get = (
-            select(RoomsORM.hotel_id)
-            .select_from(RoomsORM)
-            .filter(RoomsORM.id.in_(rooms_ids_to_get))
-        )
-        return await self.get_filtered(self.model.id.in_(hotels_ids_to_get))
