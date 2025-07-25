92fb4c88fc3992ef465174116c23a838c4f6fad7
comander <misha20061508@yandex.ru>
2025-07-25 14:56:34 +0300

    add celery and use simple task

--- app/api/facilities.py
+++ app/api/facilities.py
@@ -3,15 +3,14 @@ from fastapi_cache.decorator import cache
 
 from app.schemas.facilities import FacilityAdd
 from app.api.dependencies import DBDep
-from app.utils.decorators import cache_decorator
+from app.tasks.tasks import test_task
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 
 @router.get("")
-@cache_decorator(expire=15)
+@cache(expire=15)
 async def get_facilities(db: DBDep):
-    print("Иду в бд")
     return await db.facilities.get_all()
 
 
@@ -19,4 +18,7 @@ async def get_facilities(db: DBDep):
 async def create_facility(db: DBDep, facility_data: FacilityAdd = Body()):
     facility = await db.facilities.add(facility_data)
     await db.commit()
+
+    test_task.delay()
+
     return {"status": "OK", "data": facility}
