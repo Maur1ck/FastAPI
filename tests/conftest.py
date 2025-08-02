ca4ed424a05b9e758d4eea2ac4da4528bbaca85a
comander <misha20061508@yandex.ru>
2025-08-02 13:52:22 +0300

    refactor db and ac context managers

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,12 @@ def check_test_mode():
     assert settings.MODE == "TEST"
 
 
+@pytest.fixture(scope="function")
+async def db():
+    async with DBManager(session_factory=async_session_maker_null_poll) as db:
+        yield db
+
+
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
@@ -26,19 +32,24 @@ async def setup_database(check_test_mode):
     with open("tests/mock_rooms.json") as f:
         rooms_data = [RoomAdd.model_validate(item) for item in json.load(f)]
 
-    async with DBManager(session_factory=async_session_maker_null_poll) as db:
-        await db.hotels.add_bulk(hotels_data)
-        await db.rooms.add_bulk(rooms_data)
-        await db.commit()
+    async with DBManager(session_factory=async_session_maker_null_poll) as db_:
+        await db_.hotels.add_bulk(hotels_data)
+        await db_.rooms.add_bulk(rooms_data)
+        await db_.commit()
+
+
+@pytest.fixture(scope="session")
+async def ac():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def register_user(setup_database):
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        await ac.post(
-            "/auth/register",
-            json={
-                "email": "kot@pes.com",
-                "password": "123",
-            }
-        )
+async def register_user(ac, setup_database):
+    await ac.post(
+        "/auth/register",
+        json={
+            "email": "kot@pes.com",
+            "password": "123",
+        }
+    )
--- tests/integration_tests/hotel/test_db.py
+++ tests/integration_tests/hotel/test_db.py
@@ -1,10 +1,7 @@
-from app.database import async_session_maker_null_poll
 from app.schemas.hotels import HotelAdd
-from app.utils.db_manager import DBManager
 
 
-async def test_add_hotel():
+async def test_add_hotel(db):
     hotel_data = HotelAdd(title="Hotel 5 stars", location="New York")
-    async with DBManager(session_factory=async_session_maker_null_poll) as db:
-        new_hotel_data = await db.hotels.add(hotel_data)
-        await db.commit()
+    new_hotel_data = await db.hotels.add(hotel_data)
+    await db.commit()
