a9c701f1303114bf3039423dd5c79b0d6543be1b
comander <misha20061508@yandex.ru>
2025-08-04 20:13:27 +0300

    rewrite test_booking_crud()

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -13,22 +13,27 @@ async def test_booking_crud(db):
         user_id=user.id,
         price=100,
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
-    booking = await db.bookings.get_one_or_none(user_id=user.id, room_id=room.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == room.id
+    assert booking.user_id == user.id
 
-    new_booking_data = BookingAdd(
+    update_booking_data = BookingAdd(
         date_from=date(year=2021, month=1, day=1),
         date_to=date(year=2021, month=1, day=15),
         room_id=room.id,
         user_id=user.id,
         price=500,
     )
-    await db.bookings.edit(data=new_booking_data, room_id=room.id, user_id=user.id)
-    new_booking = await db.bookings.get_one_or_none(user_id=user.id, room_id=room.id)
-    assert new_booking
+    await db.bookings.edit(data=update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.price == 500
 
-    await db.bookings.delete(user_id=user.id, room_id=room.id)
-    booking_check = await db.bookings.get_one_or_none(user_id=user.id, room_id=room.id)
+    await db.bookings.delete(id=new_booking.id)
+    booking_check = await db.bookings.get_one_or_none(id=new_booking.id)
     assert booking_check is None
