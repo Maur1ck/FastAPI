2dcc94f3a5b669c948e37e57f852bc42c7839e2a
comander <misha20061508@yandex.ru>
2025-08-07 16:19:47 +0300

    create add_booking()

--- app/api/bookings.py
+++ app/api/bookings.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 
 from app.api.dependencies import DBDep, UserIdDep
 from app.schemas.bookings import BookingAdd, BookingAddRequest
@@ -8,10 +8,7 @@ router = APIRouter(prefix="/bookings", tags=["Бронирование"])
 
 @router.post("")
 async def create_booking(user_id: UserIdDep, booking_data: BookingAddRequest, db: DBDep):
-    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
-    price = room.price
-    _booking_data = BookingAdd(user_id=user_id, price=price, **booking_data.model_dump())
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(user_id=user_id, booking_data=booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
 
--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -1,10 +1,14 @@
 from datetime import date
 
-from sqlalchemy import select
+from fastapi import HTTPException
+from sqlalchemy import select, insert
 
+from app.models import RoomsOrm
 from app.models.bookings import BookingsOrm
 from app.repositories.base import BaseRepository
 from app.repositories.mappers.mappers import BookingDataMapper
+from app.repositories.utils import rooms_ids_for_booking
+from app.schemas.bookings import BookingAddRequest
 
 
 class BookingsRepository(BaseRepository):
@@ -18,3 +22,23 @@ class BookingsRepository(BaseRepository):
         )
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+
+    async def add_booking(self, user_id: int, booking_data: BookingAddRequest):
+        rooms_ids = rooms_ids_for_booking(date_from=booking_data.date_from, date_to=booking_data.date_to)
+        rooms_ids_free = (await self.session.execute(rooms_ids)).scalars().all()
+        print(rooms_ids_free)
+        if booking_data.room_id not in rooms_ids_free:
+            raise HTTPException(status_code=404, detail="Все номера заняты")
+        query = select(RoomsOrm).filter_by(id=booking_data.room_id)
+        room = (await self.session.execute(query)).scalar_one()
+        add_data_stmt = insert(self.model).values(
+            user_id=user_id,
+            room_id=booking_data.room_id,
+            date_from=booking_data.date_from,
+            date_to=booking_data.date_to,
+            price=room.price
+        ).returning(self.model)
+
+        result = await self.session.execute(add_data_stmt)
+        model = result.scalar_one()
+        return self.mapper.map_to_domain_entity(model)
